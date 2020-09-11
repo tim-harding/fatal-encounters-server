5f690bf804ce66ff85e0866787c994741cf7bfc0
Tim Harding <Tim@TimHarding.co>
2020-09-11 15:45:01 -0700

    Added tests of IN clause

---
+++ shared/inClause_test.go
@@ -0,0 +1,23 @@
+package shared
+
+import "testing"
+
+func TestInClause(t *testing.T) {
+	query := baseQuery()
+	where := NewWhereClause(CombinatorAnd)
+	in := NewInClause("column", []int{3, 5, 7})
+	where.AddClause(in)
+	query.AddClause(where)
+	const wanted = "SELECT a, b FROM test WHERE column IN ($1, $2, $3)"
+	try(query, wanted, t)
+}
+
+func TestIgnoreInClause(t *testing.T) {
+	query := baseQuery()
+	where := NewWhereClause(CombinatorAnd)
+	in := NewInClause("column", []int{})
+	where.AddClause(in)
+	query.AddClause(where)
+	const wanted = "SELECT a, b FROM test"
+	try(query, wanted, t)
+}
