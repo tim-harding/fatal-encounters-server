4f858e873a56315b695c5ace9951d79483ce7e3c
Tim Harding <Tim@TimHarding.co>
2020-09-11 15:58:43 -0700

    Order clause test

---
+++ shared/orderClause_test.go
@@ -0,0 +1,11 @@
+package shared
+
+import "testing"
+
+func TestOrderClause(t *testing.T) {
+	query := baseQuery()
+	order := NewOrderClause(OrderingDescending, []string{"id", "thing"})
+	query.AddClause(order)
+	const wanted = "SELECT a, b FROM test ORDER BY id, thing DESC"
+	try(query, wanted, t)
+}
